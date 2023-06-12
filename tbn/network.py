11dffd50ba58dbce2b9ba19518d722776356732c
AndreJacobsPy <andrejacobs365@gmail.com>
2023-06-12 18:11:14 -0500

    added some documentation

--- tbn/network.py
+++ tbn/network.py
@@ -2,6 +2,22 @@ import torch
 from torch import nn
 
 class BaseNet(nn.Module):
+    """
+    This is a class that helps to make building Neural Networks with PyTorch easier.
+
+    Methods:
+        train:
+            arguments:
+                xtrain: this is the inputs of model. must be in torch.Tensor dtype.
+                ytrain: this is the outputs of model. must be in torch.Tensor dtype.
+                xtest: this is inputs to test model. must be in torch.Tensor dtype.
+                ytest: this is outputs to test model. must be in torch.Tensor dtype.
+                num_epochs: how many iterations to train the model
+                optim: the torch optimizer to use. typically Adam is the best.
+                loss_fun: the loss function to minimize while training
+                lr: the learning rate of the model
+    """
+
     def __init__(self):
         super(BaseNet, self).__init__()
 
@@ -11,6 +27,48 @@ class BaseNet(nn.Module):
         return self.model(x)
     
     def train(self, x_train, y_train, x_test, y_test, num_epochs, optim, loss_fun, lr) -> dict:
+        """
+        arguments:
+                xtrain: this is the inputs of model. must be in torch.Tensor dtype.
+                ytrain: this is the outputs of model. must be in torch.Tensor dtype.
+                xtest: this is inputs to test model. must be in torch.Tensor dtype.
+                ytest: this is outputs to test model. must be in torch.Tensor dtype.
+                num_epochs: how many iterations to train the model
+                optim: the torch optimizer to use. typically Adam is the best.
+                loss_fun: the loss function to minimize while training
+                lr: the learning rate of the model
+
+        example:
+            # creating some fake data
+            xtr = torch.Tensor([[1, 2],
+                                [3, 4],
+                                [5, 6]])
+            ytr = torch.Tensor([[1.5],
+                                [3.5],
+                                [5.5]])
+
+            xte = torch.Tensor([[3, 2],
+                                [5, 1]])
+            yte = torch.Tensor([[2.5],
+                                [3.5]])
+
+            # build network
+            class MyNet(BaseNet):
+                def __init__(self):
+                    super(MyNet, self).__init__()
+
+                    self.model = torch.nn.Sequential(
+                        torch.nn.Linear(2, 8),
+                        torch.nn.ReLU(),
+                        torch.nn.Linear(8, 1)
+                    )
+
+            # train network
+            model = MyNet()
+            output = model.train(xtr, ytr, xte, yte, 5, torch.optim.Adam, torch.nn.MSELoss, 0.001)
+            print(output)
+        """
+
         loss_fn: nn.CrossEntropyLoss = loss_fun()
         optimizer: torch.optim.SGD = optim(self.parameters(), lr=lr)
 
--- test.py
+++ test.py
@@ -1,4 +1,32 @@
 from tbn.network import BaseNet
+import torch
 
 if __name__ == "__main__":
-    net = BaseNet()
+    # creating some fake data
+    xtr = torch.Tensor([[1, 2],
+                        [3, 4],
+                        [5, 6]])
+    ytr = torch.Tensor([[1.5],
+                        [3.5],
+                        [5.5]])
+
+    xte = torch.Tensor([[3, 2],
+                        [5, 1]])
+    yte = torch.Tensor([[2.5],
+                        [3.5]])
+
+    # build network
+    class MyNet(BaseNet):
+        def __init__(self):
+            super(MyNet, self).__init__()
+
+            self.model = torch.nn.Sequential(
+                torch.nn.Linear(2, 8),
+                torch.nn.ReLU(),
+                torch.nn.Linear(8, 1)
+            )
+
+    # train network
+    model = MyNet()
+    output = model.train(xtr, ytr, xte, yte, 5, torch.optim.Adam, torch.nn.MSELoss, 0.001)
+    print(output)
